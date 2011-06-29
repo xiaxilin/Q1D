9d10772831fcce415c7abfdb8105aee5fb55ff23
Joe Derlaga <joseph.derlaga@gmail.com>
2011-06-29 12:06:29 -0400

    to add primitive variable flooring function sketch

---
+++ Functions/floor_primitive_vars.f90
@@ -0,0 +1,14 @@
+function floor_primitive_vars( prim )
+
+  use set_precision, only : dp
+
+  real(dp), dimension(3), intent(in) :: prim
+  real(dp), dimension(3),            :: floor_primitive_vars
+
+  continue
+
+  floor_primitive_vars(1) = max( prim(1), 0.0001_dp )
+  floor_primitive_vars(2) = prim(2)
+  floor_primitive_vars(3) = max( prim(3), 500.0_dp )
+
+end function floor_primitive_vars
