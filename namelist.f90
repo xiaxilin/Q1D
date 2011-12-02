1ba0c682682661e00d14aa94a1677f7caebc88c5
Joe Derlaga <joseph.derlaga@gmail.com>
2011-12-02 12:50:42 -0500

    to add comments

--- namelist.f90
+++ namelist.f90
@@ -45,15 +45,34 @@ contains
 ! set defaults and read &code_control
 
     iterations = 100000
+    !sets the maximum number of iterations
+
     firstorder = 10000
+    !if running 2nd order, run this many 1st order iters to set up flow field
+
     itercheck  = 1000
+    !check for convergence every itercheck iteration
+
     iter_out   = -1
+    !output a solution every iter_out iteration, -1 if only at the end of run
+
     rkorder    = 1
+    !sets the m-step Runge-Kutta order, 1 for Euler explicit
+
     muscl      = .false.
+    !turns MUSCL scheme on or off
+
     cfl        = 1.0_dp
+    !sets the CFL number
+
     kappa      = -1.0_dp
+    !sets the type of upwinding, -1 for fully upwind
+
     toler      = 1.0e-13_dp
+    !sets the resisdual convergence tolerance
+
     limiter    = 'none'
+    !sets the type of variable limiter, only needed with MUSCLE = .true.
 
     rewind(nml_unit)
     read(nml_unit, nml=code_control)
@@ -61,8 +80,13 @@ contains
 ! set defaults and read &flux
 
     flux_type  = 'jst'
+    !sets the flux type
+
     k2 = 0.5_dp
+    !sets the adaptive dissipation for central difference flux
+
     k4 = 0.03125_dp
+    !sets the adaptive dissipation for central difference flux
 
     rewind(nml_unit)
     read(nml_unit, nml=flux)
@@ -70,10 +94,19 @@ contains
 ! set defaults and read &conditions
 
     restart = .false.
+    !start code from restart file
+
     mref  = 1.5_dp
+    !reference inflow mach number if starting from scratch
+
     to    = 600.0_dp
+    !reference total temperature, Kelvins
+
     po    = 300000.0_dp
+    !reference total pressure, Pa
+
     pback = -1.0_dp
+    !back pressure, Pa.  set less than 0 for supersonic extrapolation
 
     rewind(nml_unit)
     read(nml_unit, nml=conditions)
@@ -81,7 +114,10 @@ contains
 ! set defaults and read &gas_properties
 
     r = 287.0_dp
+    !gas constant,
+
     gamma = 1.4_dp
+    !ratio of specific heats, 7/5 for diatomic gas, 5/3 for monatomic
 
     rewind(nml_unit)
     read(nml_unit, nml=gas_properties)
