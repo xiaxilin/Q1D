e17bf51a0eeb703ffd779498ab5510fd6d01fed0
Joe Derlaga <joseph.derlaga@gmail.com>
2012-01-03 14:21:09 -0500

    to modify the block tridiagonal system to account for bc's, sill need source jacobian

--- Implicit/implicit.f90
+++ Implicit/implicit.f90
@@ -24,6 +24,7 @@ subroutine implicit_solve( cells, faces, dxsi, prim_cc, cons_cc,               &
 
   real(dp), dimension(cells+2)     :: dt
   real(dp), dimension(3,cells+2)   :: RHS, delta_cons_cc
+  real(dp), dimension(3,3)         :: DL2, DU2, bc, inv
   real(dp), dimension(3,3)         :: left_jac_L, right_jac_L
   real(dp), dimension(3,3)         :: left_jac_C, right_jac_C
   real(dp), dimension(3,3)         :: left_jac_R, right_jac_R
@@ -66,7 +67,7 @@ subroutine implicit_solve( cells, faces, dxsi, prim_cc, cons_cc,               &
                                 right_jac_R, left_jac_R )
 
       L(:,:,cell) = -right_jac_L/(dxdsi_cc(cell-1)*dxsi)
-      D(:,:,cell) = ident/dt - source_jac                                      &
+      D(:,:,cell) = ident3x3/dt - source_jac                                   &
                   + (right_jac_C-left_jac_C)/(dxdsi_cc(cell)*dxsi)
       U(:,:,cell) =  left_jac_R/(dxdsi_cc(cell+1)*dxsi)
 
@@ -87,31 +88,38 @@ subroutine implicit_solve( cells, faces, dxsi, prim_cc, cons_cc,               &
                              RHS(:,cells+2)
 
 ! Need to perform matrix modification to preserve block tri-diagonal structure
+! Needs to be made a subroutine...
+! Need to store DU2 in L(:,:,1)
+! Need to store DL2 in U(:,:,cells+2)
 
-!    bc = zero
-!    bc(1,1) = one
+! Inflow
+    bc = DU2
 
-!    call matrix_inv(3, U(:,:,2), inv)
-!    call mat_inv_3x3(U(:,:,2), inv)
+    call matrix_inv(3, U(:,:,2), inv)
+    call mat_inv_3x3(U(:,:,2), inv)
 
-!    inv = matmul(bc, inv)
+    inv = matmul(bc, inv)
 
-!    D(:,:,1) = D(:,:,1) - matmul(inv, L(:,:,2))
-!    U(:,:,1) = U(:,:,1) - matmul(inv, D(:,:,2))
-!    RHS(:,1) = RHS(:,1) - matmul(inv, RHS(:,2))
+    D(:,:,1) = D(:,:,1) - matmul(inv, L(:,:,2))
+    U(:,:,1) = U(:,:,1) - matmul(inv, D(:,:,2))
+    RHS(:,1) = RHS(:,1) - matmul(inv, RHS(:,2))
 
-!    call matrix_inv(3, L(:,:,y_nodes-1), inv)
-!    call mat_inv_3x3(L(:,:,y_nodes-1), inv)
+! Outflow
+    bc = DL2
 
-!    inv = matmul(bc, inv)
+    call matrix_inv(3, L(:,:,cells+1), inv)
+    call mat_inv_3x3(L(:,:,cells+1), inv)
 
-!    L(:,:,y_nodes) = L(:,:,y_nodes) - matmul(inv, D(:,:,y_nodes-1))
-!    D(:,:,y_nodes) = D(:,:,y_nodes) - matmul(inv, U(:,:,y_nodes-1))
-!    RHS(:,y_nodes) = RHS(:,y_nodes) - matmul(inv, RHS(:,y_nodes-1))
+    inv = matmul(bc, inv)
+
+    L(:,:,cells+2) = L(:,:,cells+2) - matmul(inv, D(:,:,cells+1))
+    D(:,:,cells+2) = D(:,:,cells+2) - matmul(inv, U(:,:,cells+1))
+    RHS(:,cells+2) = RHS(:,cells+2) - matmul(inv, RHS(:,cells+1))
 
 ! solve the system of equations
     call triblocksolve(3, cells+2, L, D, U, RHS, delta_cons_cc)
 
+! Update the conserved variables
     cons_cc = cons_cc+deltat_cons_cc
 
     if (mod(n,itercheck) == 0) then
