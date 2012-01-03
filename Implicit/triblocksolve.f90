e69d4441d2d68d0198a6af1b3d5d2a78cb079841
Joe Derlaga <joseph.derlaga@gmail.com>
2012-01-03 14:25:34 -0500

    changed nomenclature

--- Implicit/triblocksolve.f90
+++ Implicit/triblocksolve.f90
@@ -1,13 +1,13 @@
-subroutine triblocksolve(neq, dof, L, D, U, RHS, x)
+subroutine triblocksolve(neq, dof, LD, DD, UD, RHS, soln)
 
   use set_precision, only : dp
 
   implicit none
 
   integer,                          intent(in)    :: neq, dof
-  real(dp), dimension(neq,neq,dof), intent(inout) :: L, D, U
+  real(dp), dimension(neq,neq,dof), intent(inout) :: LD, DD, UD
   real(dp), dimension(neq,dof),     intent(inout) :: RHS
-  real(dp), dimension(neq,dof),     intent(out)   :: x
+  real(dp), dimension(neq,dof),     intent(out)   :: soln
 
   integer :: i
 
@@ -16,26 +16,26 @@ subroutine triblocksolve(neq, dof, L, D, U, RHS, x)
 
   continue
 
-  A(:,:) = D(:,:,1)
+  A(:,:) = DD(:,:,1)
   y(:,1) = RHS(:,1)
 
   do i = 2, dof
     AT = transpose(A)
     call matrix_inv(neq, AT, AT_inv)
 
-    D(:,:,i-1) = transpose(AT_inv)
-    temp = transpose( matmul( AT_inv, transpose(L(:,:,i)) ) ) 
+    DD(:,:,i-1) = transpose(AT_inv)
+    temp = transpose( matmul( AT_inv, transpose(LD(:,:,i)) ) ) 
 
-    A      = D(:,:,i) - matmul(temp, U(:,:,i-1))
-    y(:,i) = RHS(:,i) - matmul(temp, y(:,i-1))
+    A      = DD(:,:,i) - matmul(temp, UD(:,:,i-1))
+    y(:,i) = RHS(:,i)  - matmul(temp, y(:,i-1))
   end do
 
   call matrix_inv(neq, A, AT_inv)
 
-  x(:,dof) = matmul(AT_inv, y(:,dof))
+  soln(:,dof) = matmul(AT_inv, y(:,dof))
 
   do i = dof-1,1,-1
-    x(:,i) = matmul( D(:,:,i), (y(:,i) - matmul( U(:,:,i), x(:,i+1) )) )
+    soln(:,i) = matmul( DD(:,:,i), (y(:,i) - matmul( UD(:,:,i), soln(:,i+1) )) )
   end do
 
 end subroutine triblocksolve
