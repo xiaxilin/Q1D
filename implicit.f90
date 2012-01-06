6087fbfdb139385092de73d80bbb21390e5861d1
Joe Derlaga <joseph.derlaga@gmail.com>
2012-01-06 13:17:43 -0500

    to use the new jacobian and try to figure out what's going wrong, source jacobian is correct, flux jacobians seem to be correct, need to check how the RHS is behaving

--- implicit.f90
+++ implicit.f90
@@ -22,7 +22,7 @@ contains
     use solvers,         only : iterations, itercheck, create_residual, &
                                 check_convergence, set_time_step
     use bc,              only : subsonic_inflow, supersonic_outflow
-    use jacobians,       only : jac_vanleer_1D
+    use jacobians,       only : new_jac_vanleer_1D
     use matrix_manip,    only : triblocksolve, mat_inv_3x3, matrix_inv
     use write_soln,      only : write_soln_line
 
@@ -69,32 +69,36 @@ contains
       call subsonic_inflow( cons_cc(:,1), cons_cc(:,2), cons_cc(:,3),          &
                             D(:,:,1), U(:,:,1), L(:,:,1), RHS(:,1) )
 
-      L(:,:,1) = L(:,:,1)/(dxdxsi_cc(3)*dxsi*area_cc(3))
-      D(:,:,1) = D(:,:,1)/(dxdxsi_cc(2)*dxsi*area_cc(2))
-      U(:,:,1) = U(:,:,1)/(dxdxsi_cc(2)*dxsi*area_cc(2))
+!      L(:,:,1) = L(:,:,1)/(dxdxsi_cc(3)*dxsi*area_cc(3))
+!      D(:,:,1) = D(:,:,1)/(dxdxsi_cc(2)*dxsi*area_cc(2))
+!      U(:,:,1) = U(:,:,1)/(dxdxsi_cc(2)*dxsi*area_cc(2))
       
 ! calculate Jacobians for inflow face
 
-      call jac_vanleer_1D( cons_cc(:,1), cons_cc(:,2), right_jac_L, left_jac_C)
+      call new_jac_vanleer_1D( cons_cc(:,1), cons_cc(:,2), right_jac_L, left_jac_C)
 
       do cell = 2, cells+1
 
-        call jac_vanleer_1D( cons_cc(:,cell), cons_cc(:,cell+1),               &
+        call new_jac_vanleer_1D( cons_cc(:,cell), cons_cc(:,cell+1),           &
                                   right_jac_C, left_jac_R )
 
         source_jac = zero
         source_jac(2,1) = half*gm1*prim_cc(2,cell)**2
         source_jac(2,2) = -gm1*prim_cc(2,cell)
         source_jac(2,3) = gm1
-        source_jac = source_jac*dadx_cc(cell)
+        source_jac = source_jac*dadx_cc(cell)/area_cc(cell)
 
         divisor = dxdxsi_cc(cell)*dxsi*area_cc(cell)
 
         L(:,:,cell) = -right_jac_L*area_f(cell-1) / divisor
         D(:,:,cell) = ident3x3/dt(cell)                                        &
                     + ( right_jac_C*area_f(cell)-left_jac_C*area_f(cell-1) )   &
-                    / divisor - source_jac / area_cc(cell)
+                    / divisor - source_jac
         U(:,:,cell) =  left_jac_R*area_f(cell) / divisor
+
+!        L(:,:,cell) = -right_jac_L
+!        D(:,:,cell) = ident3x3/dt(cell) + ( right_jac_C-left_jac_C )
+!        U(:,:,cell) =  left_jac_R
 
 ! shift Jacobians to avoid recalculation
 
@@ -110,31 +114,31 @@ contains
                                D(:,:,cells+2), L(:,:,cells+2), U(:,:,cells+2), &
                                RHS(:,cells+2) )
 
-      L(:,:,cells+2) = L(:,:,cells+2)/(dxdxsi_cc(cells+1)*dxsi*area_cc(cells+1))
-      D(:,:,cells+2) = D(:,:,cells+2)/(dxdxsi_cc(cells+1)*dxsi*area_cc(cells+1))
-      U(:,:,cells+2) = U(:,:,cells+2)/(dxdxsi_cc(cells)*dxsi*area_cc(cells))
+!      L(:,:,cells+2) = L(:,:,cells+2)/(dxdxsi_cc(cells+1)*dxsi*area_cc(cells+1))
+!      D(:,:,cells+2) = D(:,:,cells+2)/(dxdxsi_cc(cells+1)*dxsi*area_cc(cells+1))
+!      U(:,:,cells+2) = U(:,:,cells+2)/(dxdxsi_cc(cells)*dxsi*area_cc(cells))
       
 ! Modify matrix to maintain block tridiagonal structure
 ! Needs to be made a subroutine...
 
 ! Inflow
 !      call matrix_inv(3, U(:,:,2), inv)
-      call mat_inv_3x3(U(:,:,2), inv)
-      inv = matmul(L(:,:,1), inv)
+!      call mat_inv_3x3(U(:,:,2), inv)
+!      inv = matmul(L(:,:,1), inv)
 
-      D(:,:,1) = D(:,:,1) - matmul(inv, L(:,:,2))
-      U(:,:,1) = U(:,:,1) - matmul(inv, D(:,:,2))
-      RHS(:,1) = RHS(:,1) - matmul(inv, RHS(:,2))
+!      D(:,:,1) = D(:,:,1) - matmul(inv, L(:,:,2))
+!      U(:,:,1) = U(:,:,1) - matmul(inv, D(:,:,2))
+!      RHS(:,1) = RHS(:,1) - matmul(inv, RHS(:,2))
 
 ! Outflow
 !      call matrix_inv(3, L(:,:,cells+1), inv)
-      call mat_inv_3x3(L(:,:,cells+1), inv)
+!      call mat_inv_3x3(L(:,:,cells+1), inv)
 
-      inv = matmul(U(:,:,cells+2), inv)
+!      inv = matmul(U(:,:,cells+2), inv)
 
-      L(:,:,cells+2) = L(:,:,cells+2) - matmul(inv, D(:,:,cells+1))
-      D(:,:,cells+2) = D(:,:,cells+2) - matmul(inv, U(:,:,cells+1))
-      RHS(:,cells+2) = RHS(:,cells+2) - matmul(inv, RHS(:,cells+1))
+!      L(:,:,cells+2) = L(:,:,cells+2) - matmul(inv, D(:,:,cells+1))
+!      D(:,:,cells+2) = D(:,:,cells+2) - matmul(inv, U(:,:,cells+1))
+!      RHS(:,cells+2) = RHS(:,cells+2) - matmul(inv, RHS(:,cells+1))
 
 ! solve the system of equations
       call triblocksolve(3, cells+2, L, D, U, RHS, delta_cons_cc)
@@ -142,14 +146,25 @@ contains
 ! Update the conserved variables
       cons_cc = cons_cc+delta_cons_cc
 
+!      do cell = 1, cells+2
+!        print*, cons_cc(:,cell)
+!      end do
+
       if (mod(n,itercheck) == 0) then
         call check_convergence(cells, n, RHS, convergence_flag)
       end if
 
+! to attempt floors
       do cell = 1, cells+2
         prim_cc(:,cell) = conserved_to_primitive_1D(cons_cc(:,cell))
+        prim_cc(1,cell) = max(prim_cc(1,cell), 0.0001_dp)
+        prim_cc(2,cell) = max(prim_cc(2,cell), 5.0_dp)
+        prim_cc(3,cell) = max(prim_cc(3,cell), 500.0_dp)
+        cons_cc(:,cell) = primitive_to_conserved_1D(prim_cc(:,cell))
       end do
-
+!      do cell = 1, cells+2
+!        print*, cons_cc(:,cell)
+!      end do
 !      if (iter_out >= 0 .and. mod(n,iter_out) == 0) then
 !        call write_soln_line(n, cells, x_cc, prim_cc, cons_cc)
 !      end if
@@ -174,5 +189,6 @@ contains
   end subroutine implicit_solve
 
   include 'conserved_to_primitive_1D.f90'
+  include 'primitive_to_conserved_1D.f90'
 
 end module implicit
