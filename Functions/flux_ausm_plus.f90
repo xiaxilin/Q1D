ac7df5ad8c917e2b5534ef6c2c13b95ce3817443
Joe Derlaga <joseph.derlaga@gmail.com>
2012-04-09 15:39:29 -0400

    to fix some errors such as integer instead of real and wrong momentum combination, need to find stability regime for AUSM/AUSM+

--- Functions/flux_ausm_plus.f90
+++ Functions/flux_ausm_plus.f90
@@ -8,18 +8,19 @@
 
   use set_precision,   only : dp
   use set_constants,   only : fourth, half, one, two
+  use fluid_constants, only : gm1, xgp1
 
   implicit none
 
   real(dp), dimension(3), intent(in)  :: prim_L, prim_R
   real(dp), dimension(3)              :: flux_ausm_plus, fL, fR, cons_L, cons_R
 
-  real(dp) :: rhoL, aL, uL, PL, HTL, rhoR, aR, uR, PR, HTR
+  real(dp) :: aL, aR, rhoL, uL, PL, HTL, rhoR, uR, PR, HTR
   real(dp) :: a_star, aL_tilde, aR_tilde, a_half, m_half, p_half
   real(dp) :: ML, ML_script, PL_script, MR, MR_script, PR_script
 
-  real(dp), parameter :: alpha = 1.0_dp/8.0_dp
-  real(dp), parameter :: beta  = 3.0_dp/16.0_dp
+  real(dp), parameter :: alpha = 3.0_dp/16.0_dp
+  real(dp), parameter :: beta  = 1.0_dp/8.0_dp
 
   continue
 
@@ -27,7 +28,7 @@
   rhoL = prim_L(1)
   uL   = prim_L(2)
   PL   = prim_L(3)
-  aL   = speed_of_sound(PL, rhoL)
+  aL   = speed_of_sound(pL,rhoL)
 
   cons_L = primitive_to_conserved_1D(prim_L)
 
@@ -37,19 +38,21 @@
   rhoR = prim_R(1)
   uR   = prim_R(2)
   PR   = prim_R(3)
-  aR   = speed_of_sound(PR, rhoR)
+  aR   = speed_of_sound(pR,rhoR)
 
   cons_R = primitive_to_conserved_1D(prim_R)
 
   HTR = (cons_R(3)+PR)/rhoR
 
 !Interface speed of sound and mach numbers
-  a_star = sqrt(uL*uR)
+  a_star = sqrt(aL*aR)
 
-  aL_tilde = a_star**2/max(a_star, abs(uL))
-  aR_tilde = a_star**2/max(a_star, abs(uR))
+!  aL_tilde = a_star**2/max(a_star, abs(uL))
+!  aR_tilde = a_star**2/max(a_star, abs(uR))
 
-  a_half = min(aL_tilde, aR_tilde)
+!  a_half = min(aL_tilde, aR_tilde)
+
+  a_half = a_star
 
   ML = uL/a_half
   MR = uR/a_half
@@ -57,8 +60,8 @@
 !Get other interface properties
 
 !Left side
-  if (abs(Ml)<1.0_dp) then
-    ML_script = half*(ML+one)**2 + beta*(ML**2-1)**2
+  if (abs(ML)<1.0_dp) then
+    ML_script = half*(ML+one)**2 + beta*(ML**2-one)**2
     PL_script = fourth*(two-ML)*(ML+one)**2 + alpha*ML*(ML**2-one)**2
   else
     ML_script = half*(ML+abs(ML))
@@ -66,8 +69,8 @@
   end if
 
 !Right side
-  if (abs(Mr)<1.0_dp) then
-    MR_script = -half*(MR-one)**2 - beta*(MR**2-1)**2
+  if (abs(MR)<1.0_dp) then
+    MR_script = -half*(MR-one)**2 - beta*(MR**2-one)**2
     PR_script = fourth*(two+MR)*(MR-one)**2 - alpha*MR*(MR**2-one)**2
   else
     MR_script = half*(MR-abs(MR))
@@ -78,7 +81,7 @@
   p_half = PL_script*PL + PR_script*PR
 
   FL = (/rhoL, rhoL*uL, rhoL*HTL/)
-  FR = (/rhoR, rhoL*uR, rhoR*HTR/)
+  FR = (/rhoR, rhoR*uR, rhoR*HTR/)
 
   flux_ausm_plus = half*a_half*(m_half*(FL+FR)-abs(m_half)*(FR-FL))
   flux_ausm_plus(2) = flux_ausm_plus(2) + p_half
